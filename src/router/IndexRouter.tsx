6d20ef9ec06f6a57927e6b846b0797de6e4ee3dc
xubangjun <bangjun.xu@ucdconnect.ie>
2024-11-17 11:05:42 +0000

    new

---
+++ src/router/IndexRouter.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import { HashRouter, Route, Routes } from "react-router-dom";
+import NewsSandBox from "../view/sandbox/NewsSandBox";
+const IndexRouter: React.FC = () => {
+  return (
+    <HashRouter>
+      <Routes>
+        {/* Wrap all Route elements inside a Routes component */}
+        {/* <Route path="/" element={<NewsSandBox />} /> */}
+        <Route path="/" element ={<NewsSandBox/>}/>
+      </Routes>
+    </HashRouter>
+  );
+};
+
+export default IndexRouter;
