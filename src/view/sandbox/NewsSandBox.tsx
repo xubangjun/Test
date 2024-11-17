7a5435723128e702409cb9a19232a5be03ae2cba
xubangjun <bangjun.xu@ucdconnect.ie>
2024-11-17 11:26:42 +0000

    t

--- src/view/sandbox/NewsSandBox.tsx
+++ src/view/sandbox/NewsSandBox.tsx
@@ -3,7 +3,7 @@ import Box from '@mui/material/Box';
 function NewsSandBox() {
     return (
         <Box component="section" sx={{ p: 2, border: '1px dashed grey' }}>
-          This Box renders as an HTML section element.
+           Bad-Ass Louis.
         </Box>
       );
 }
