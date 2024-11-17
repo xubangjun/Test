a833244d04106a3b789ce56a1cf21a325ffab502
xubangjun <bangjun.xu@ucdconnect.ie>
2024-11-17 11:23:28 +0000

    mui

--- src/view/sandbox/NewsSandBox.tsx
+++ src/view/sandbox/NewsSandBox.tsx
@@ -1,4 +1,10 @@
+import Box from '@mui/material/Box';
+
 function NewsSandBox() {
-  return <div> test </div>
+    return (
+        <Box component="section" sx={{ p: 2, border: '1px dashed grey' }}>
+          This Box renders as an HTML section element.
+        </Box>
+      );
 }
 export default NewsSandBox;
