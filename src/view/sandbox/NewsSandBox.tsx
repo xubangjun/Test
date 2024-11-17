3ee2586dbdb59371bc824da31a0649d0dd9007a8
xubangjun <bangjun.xu@ucdconnect.ie>
2024-11-17 18:01:02 +0000

    more info

--- src/view/sandbox/NewsSandBox.tsx
+++ src/view/sandbox/NewsSandBox.tsx
@@ -1,10 +1,10 @@
 import { Route, Routes } from "react-router-dom";
-// import TopHeader from "../../components/sandbox/TopHeader";
+import TopHeader from "../../components/sandbox/TopHeader";
 import Content from "./Content";
 function NewsSandBox() {
     return(
         <div>
-            {/* <TopHeader></TopHeader> */}
+            <TopHeader></TopHeader>
             <Content />
             <Routes>
                 <Route path="home" element={<Content />} />
