a3e0c0a90b0216d7c01c56f5fcc62962c1b8b4ca
xubangjun <bangjun.xu@ucdconnect.ie>
2024-11-17 21:59:21 +0000

    more info

--- src/view/sandbox/Content.tsx
+++ src/view/sandbox/Content.tsx
@@ -6,6 +6,13 @@ function Content() {
         <Box>
           <img src="images/2.jpeg" alt="son" style={{ width: '300px', height: 'auto' }} />
         </Box>
+        <div>
+        <video controls style={{ width: "100%", marginTop: "20px" }}>
+          <source src={"images/videoplayback.mp4"} type="video/mp4" />
+          Your browser does not support the video tag.
+        </video>
+        </div>
+        
         <Box>
           123
         </Box>
